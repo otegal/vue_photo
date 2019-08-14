4d9f607c0027f10860cd60b238f780f4f8c9140a
h-hiroki <gyouzakuuka@gmail.com>
2019-08-14 15:30:30 +0900

    ヘッダーフッターの出し分け

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -3,17 +3,20 @@
     <RouterLink class="navbar__brand" to="/">
       Vue photo
     </RouterLink>
+
     <div class="navbar__menu">
-      <div class="navbar__item">
+      <div v-if="isLogin" class="navbar__item">
         <button class="button">
           <i class="icon ion-md-add"></i>
           Submit a photo
         </button>
       </div>
-      <span class="navbar__item">
-        username
+
+      <span v-if="isLogin" class="navbar__item">
+        {{ username }}
       </span>
-      <div class="navbar__item">
+
+      <div v-else class="navbar__item">
         <RouterLink class="button button--link" to="/login">
           Login / Register
         </RouterLink>
@@ -21,3 +24,16 @@
     </div>
   </nav>
 </template>
+
+<script>
+export default {
+  computed: {
+    isLogin() {
+      return this.$store.getters['auth/check']
+    },
+    username() {
+      return this.$store.getters['auth/username']
+    }
+  }
+}
+</script>
